02bbbadbb31bd5fbfb7fe3e8f4d5d5e70cbdfea1
Philip Lambok <philiplambok71@gmail.com>
2021-06-12 18:41:26 +0700

    Add destroy stuff

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -25,7 +25,16 @@ class ChatsController < ApplicationController
       end
     end
   end
-  
+
+  def destroy
+    chat = Chat.find params[:id]
+    chat.destroy!
+    respond_to do |format|
+      format.html do
+        redirect_to root_path
+      end
+    end
+  end
 
   private
 
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,4 @@
 class Chat < ApplicationRecord
-  after_create { broadcast_append_to "chats" }
+  after_create { broadcast_append_to 'chats' }
+  after_destroy { broadcast_remove_to 'chats' }
 end
